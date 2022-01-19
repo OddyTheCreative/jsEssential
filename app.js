f733e3eb0b9b77b8936e0b540ee58ffb48828372
jihun <oddythecreative@gmail.com>
2022-01-19 13:48:48 +0900

    feat: Add 11 lines of code to start a project

---
+++ app.js
@@ -0,0 +1,18 @@
+const ajax = new XMLHttpRequest();
+const newsUrl = "https://api.hnpwa.com/v0/news/1.json";
+
+ajax.open("GET", newsUrl, false);
+ajax.send();
+
+// response 객체로 바꾸는 코드
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < newsFeed.length; i++) {
+  //   let li = document.createElement(`<li>${newsFeed[i].title}</li>`);
+  const li = document.createElement("li");
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
